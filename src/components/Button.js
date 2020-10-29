7d5b63738e1a76419e99ee8a282d9a81d6a4a4f1
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2020-10-29 16:57:11 +0000

    feat: added profile page

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Button({text, type, to}) {
+export default function Button({text, type, to, className}) {
   return (
-    <Link to={ to ? to : '/login' }>
+    <Link to={ to ? to : '/login' } className={className ? className : ''}> 
       <button type={ type ? type : 'text' } className='btn-header' style={{ color: '#F5F5F5' }}>{text ? text : 'Login'}</button>
     </Link>
   )
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Container from '../components/Container'
+import Button from '../components/Button'
 
 function App() {
   document.title = "Foodly | Login"
@@ -8,7 +9,28 @@ function App() {
     <>
       <Header />
       <Container className="profile-container">
-        <div>
+        <div className='profile-header'>
+          <div className='profile-header__image' />
+          <div className='profile-header__info'>
+            <h1>
+              Pedro Lopes
+            </h1>
+            <h4>
+              23 anos
+            </h4>
+            <h4>
+              Leiria
+            </h4>
+          </div>
+          <Button className='profile-header__editBtn' text='Editar'/>
+        </div>
+        <div className='profile-body'>
+          <h1>
+            Description
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis ad ratione, reprehenderit rerum inventore porro quas, magnam accusantium nemo nobis, dolore corporis nam delectus. Consequuntur, vero! Dolorem expedita explicabo cum?
+          </p>
         </div>
       </Container>
     </>
